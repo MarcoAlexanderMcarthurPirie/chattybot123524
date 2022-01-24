52c8f14d083ee750a2f0ccaa5cc1ec84eed94395
MarcoAlexanderMcarthurPirie <marco.pirie@online.island.edu.hk>
2022-01-24 14:30:47 +0800

    Initial commit

---
+++ Topics/Integer types and operations/First digit/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        int sum = n/10;
+        
+        System.out.println(sum);
+    }
+}
